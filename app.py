c9018a83585dddeb350f5f5ffacc96f992854e49
SANJAI_3 <sanjaisam333@gmail.com>
2023-05-20 17:36:04 +0530

    new

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 from pptx import Presentation
 from pptx.dml.color import RGBColor
 from pptx.util import Pt
-from text_gen import get_response
-from image import get_image
+
+from pexels_api import API
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the secret key from the environment variable
+
+pixel_api=os.getenv("PIXELS_API")
+
+pexels_api = API(pixel_api)
+
+
+
+def get_image(topic):
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    photos = pexels_api.search(topic)
+    for index, photo in enumerate(photos['photos'], start=1):
+        image_url = photo['src']['large']
+        image_id = photo['id']
+        image_filename = f'images/{index}.jpg'  # Save the images in the "images" directory
+
+        response = requests.get(image_url)
+        response.raise_for_status()
+
+        with open(image_filename, 'wb') as file:
+            file.write(response.content)
+
+        print(f"Image saved: {image_filename}")
+
+
 import streamlit as st
 from present_ai import get_presentation
 
